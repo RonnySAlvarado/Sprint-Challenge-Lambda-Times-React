c82c90bbd245334c99e3d497b79a5178faed1227
Ronny Alvarado <rsalvarado777@gmail.com>
2019-03-23 13:18:30 -0500

    refactored cards.js and card.js to use styled-components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardsContainerStyle = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardsContainerStyle>
       {props.cards.map((eachCard, index) => <Card eachCard={eachCard} key={index} />)}
-    </div>
+    </CardsContainerStyle>
   )
 }
 
